c29223b910d0acaa03e3ec477bc2c5cde8a1e763
Stan Rozenraukh <stan@stanistan.com>
2012-03-10 12:52:17 -0500

    tests for \Def, \Element

---
+++ tests/helpers.php
@@ -0,0 +1,71 @@
+<?
+
+require __DIR__ . '/../bootstrap.php';
+
+class testHelpers extends PHPUnit_Framework_TestCase {
+
+	public function testBasicDefhtml() {
+
+		$basic = \Hiccup\Def\html('span');
+		$this->assertEquals(
+			$basic('foo'), 
+			'<span>foo</span>');
+
+		$more = \Hiccup\Def\html('div#container', array('p', 'oh hi'));
+		$this->assertEquals(
+			$more(array('p#my-p', 'more')),
+			'<div id="container"><p>oh hi</p><p id="my-p">more</p></div>');
+
+	}
+
+	public function testJS() {
+		$this->assertEquals(
+			\Hiccup\Element\javascriptTag("alert('hi');"), 
+			'<script type="text/javascript">alert(\'hi\');</script>');
+	}
+
+	public function testLinkTo() {
+		$this->assertEquals(
+			\Hiccup\Element\linkTo('/'),
+			'<a href="/"></a>', 'no text');
+
+		$this->assertEquals(
+			\Hiccup\Element\linkTo('/', 'some'),
+			'<a href="/">some</a>');
+
+		$this->assertEquals(
+			\Hiccup\Element\linkTo('/', 'some ', 'text'),
+			'<a href="/">some text</a>');
+
+		$this->assertEquals(
+			\Hiccup\Element\linkTo('/', 'some ', array('img#img')),
+			'<a href="/">some <img id="img" /></a>');
+	}
+
+	public function testMailTo() {
+		$this->assertEquals(
+			\Hiccup\Element\mailTo('foo@example.com'),
+			'<a href="mailto:foo@example.com">foo@example.com</a>');
+
+		$this->assertEquals(
+			\Hiccup\Element\mailTo('foo@example.com', 'foo'),
+			'<a href="mailto:foo@example.com">foo</a>');
+
+		$this->assertEquals(
+			\Hiccup\Element\mailTo('foo@example.com', 'Stan ', 'isTan'),
+			html('a', array('href' => 'mailto:foo@example.com'), 'Stan isTan'));
+	}
+
+	public function testLists() {
+		
+		$this->assertEquals(
+			\Hiccup\Element\ol('foo', 'bar', 'baz'),
+			html('ol', array('li', 'foo'), array('li', 'bar'), array('li', 'baz')));
+
+		$this->assertEquals(
+			\Hiccup\Element\ul('foo', 'bar', 'baz'),
+			html('ul', array('li', 'foo'), array('li', 'bar'), array('li', 'baz')));
+
+	}
+
+}
